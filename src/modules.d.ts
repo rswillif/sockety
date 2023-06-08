061cb39c17fa984038ad7cf7b95986dd4cfae50f
Robert Williford <retro490@gmail.com>
2023-06-07 21:18:41 -0600

    comment cleanup

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,9 +1,7 @@
-// require("workerpool");
 require('./helpers/requestHelper');
 require("./helpers/responseHelper");
 require("./helpers/appHelper");
 
-// declare module "workerpool";
 declare module "requestHelper";
 declare module "responseHelper";
 declare module "appHelper";
